37b11d48cb66f81d197ec57ee490fc88330bfdde
poispois <dev@pois.jp>
2020-12-02 02:19:14 +0900

    Update suite.py

--- graal-js/mx.graal-js/suite.py
+++ graal-js/mx.graal-js/suite.py
@@ -24,7 +24,7 @@ suite = {
         {
            "name" : "regex",
            "subdir" : True,
-           "version" : "5a7599b74cfe66e595b6f26046eb9503b40c2bb8",
+           "version" : "16fbfc5e623305901367f77b0ec47dfa58ef9a63",
            "urls" : [
                 {"url" : "https://github.com/prg-titech/graal.git", "kind" : "git"},
                 {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
